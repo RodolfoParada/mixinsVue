1044a39d59b782d59f8368cf28e8636c8423540e
Rodolfo <rodolfo.parada.gonzalez@gmail.com>
2023-06-13 22:40:10 -0400

    ejemplo de mixins

---
+++ src/components/AComponent.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>A Component</h1>
+        <p>{{appVersion}}</p>
+    </div>
+</template>
+
+<script>
+import BaseComponent from './BaseComponent.vue'
+export default {
+    name: 'AComponent',
+    mixins: [BaseComponent]
+}
+</script>
---
+++ src/components/BComponent.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1>B Component</h1>
+        <button @click="clickHandler">Click me</button>
+        <p>{{appVersion}}</p>
+    </div>
+</template>
+
+<script>
+import BaseComponent from './BaseComponent.vue'
+export default {
+    name: 'BComponent',
+    mixins: [BaseComponent],
+    methods: {
+        clickHandler() {
+            this.appVersion = "1.2.4"
+        }
+    }
+}
+</script>
